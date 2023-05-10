3944be10b8bab2a194347f32d4f71a4cf269895e
Prooheckcp <vasco.soares.2001@gmail.com>
2023-05-10 22:27:33 +0100

    Create model methods

--- rogoose/Classes/Model.lua
+++ rogoose/Classes/Model.lua
@@ -2,12 +2,12 @@
 local Schema = require(script.Parent.Schema)
 local InternalSignals = require(script.Parent.Parent.Constants.InternalSignals)
 local Profile = require(script.Parent.Profile)
-local GeneratePlayerKey 
+local GeneratePlayerKey = require(script.Parent.Parent.Functions.GeneratePlayerKey)
 
 local Model = {}
 Model.__index = Model
 Model.type = "DatabaseModel"
-Model._profiles = {}
+Model._profiles = {} :: {[string]: Profile.Profile}
 
 function Model.new(modelName: string, schema: Schema.Schema): Model
     local self = setmetatable({}, Model)
@@ -20,14 +20,36 @@ function Model.new(modelName: string, schema: Schema.Schema): Model
     return self
 end
 
-function Model:Create(key: Player | string, startingData: {[string]: any}): Profile
+function Model:Create(key: Player | string, startingData: {[string]: any}): Profile.Profile
     local finalKey: string = ""
 
     if typeof(key) == "Instance" and key:IsA("Player") then
-
+        finalKey = GeneratePlayerKey(key)
     else
         finalKey = key
     end
+
+    local newProfile: Profile.Profile = Profile.new()
+    newProfile._data = startingData
+    self._profiles[finalKey] = newProfile
+
+    return newProfile
+end
+
+function Model:Find(key: Player | string): Profile.Profile?
+    return self._profiles[key]
+end
+
+function Model:_GetKey(key: Player | string): string
+    local finalKey: string = ""
+
+    if typeof(key) == "Instance" and key:IsA("Player") then
+        finalKey = GeneratePlayerKey(key)
+    else
+        finalKey = key
+    end
+
+    return finalKey
 end
 
 function Model.__tostring(model: Model): string
