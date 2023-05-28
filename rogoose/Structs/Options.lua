eaa7e142ec48eecf70383eabe7f01f7c2d5fcf91
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2023-05-28 18:59:41 +0100

    Finishing Options struct

--- rogoose/Structs/Options.lua
+++ rogoose/Structs/Options.lua
@@ -1,8 +1,23 @@
 --!strict
 local ModelType = require(script.Parent.Parent.Enums.ModelType)
 
+--[=[
+    @class Options
+    @tag Structs
+
+    The options consist of a simple struct to feed the options you want into
+    the model when you create it. This options will change the behavior of said model
+]=]
 local Options = {}
 Options.__index = Options
+Options.type = "Options"
+
+---@prop scope string -- The scope of the model. This will be fed into the DataStoreService:GetDataStore() function
+Options.scope = "global" :: string
+--@prop options Instance? -- The options to feed into the DataStoreService:GetDataStore() function
+Options.options = nil :: Instance?
+--@prop modelType ModelType -- The type of model you want to create. Using Player as a key will result in a player specific model
+Options.modelType = ModelType.Player :: ModelType.ModelType
 
 export type Options = {
     scope: string,
@@ -10,6 +25,26 @@ export type Options = {
     modelType: ModelType.ModelType,
 }
 
+--[=[
+    Creates a new instance of Options
+
+    E.g
+
+    ```lua
+        local Options = RoGoose.Options
+        local options = Options.new({
+            scope = "global",
+            options = nil,
+            modelType = RoGoose.ModelType.Player
+        })
+
+        RoGoose.Model.create("Example", schema, options)
+    ```
+
+    @param options Options? -- The options to create the instance with
+
+    @return Options
+]=]
 function Options.new(options: Options?): Options
     options = (options or {}) :: Options
 
